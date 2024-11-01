02a4902ce1d557c878d70b5cb72aa952fdeae171
Tinoco06 <tinocojorge6>
2024-11-01 12:29:02 -0600

    Configuración inicial y despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,14 @@
 import { defineConfig } from "vite";
-import * as glob from "glob";
-import path, { resolve } from "node:path";
 import htmlPurge from 'vite-plugin-purgecss';
 
-const getHtmlEntries = ()=>{
-    return Object.fromEntries(
-        [
-            ...glob.sync('./**/*.html', { ignore:['./dist/**','./node_modules/**']}).map(file=>[
-                file.slice(0, file.length - path.extname(file).length),
-                resolve(__dirname, file)
-            ])
-        ]
-    )
-}
-
-export default defineConfig(
-    {
-        appType: 'mpa',
-        base: "/VitePortafolio/",
-        build: {
-            rollupOptions: {
-              input: {
-                main: './index.html'  
-              }
-            }},
-        plugins: [
-            htmlPurge({})
-        ]
-    }
-);
+export default defineConfig({
+    base: "/VitePortafolio/",
+    build: {
+        rollupOptions: {
+            input: './index.html'
+        }
+    },
+    plugins: [
+        htmlPurge({})
+    ]
+});
