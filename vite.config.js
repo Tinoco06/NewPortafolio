a783a1416dfaf63ef2a3986eb6052fecce0804db
Tinoco06 <tinocojorge6>
2024-11-01 12:41:23 -0600

    Reintenta despliegue en GitHub Pages 2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import htmlPurge from 'vite-plugin-purgecss';
 
 export default defineConfig({
-    base: "/VitePortafolio/",
+    base: "/PortafolioConLess/",
     build: {
         rollupOptions: {
             input: './index.html'
